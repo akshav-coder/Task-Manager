746f065b6d0716c96b3c4c5a8ee330c52a9deafe
Akshav M <akshavm@Akshavs-MacBook-Pro.local>
2024-10-11 17:22:50 +0530

    add url in env

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5001/api/",
+    baseUrl: import.meta.env.VITE_BASE_URL,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("user")
         ? JSON.parse(localStorage.getItem("user")).token
